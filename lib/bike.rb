8cf42f6164ba40b47d7579bd7e5d1a0b6c0b98ab
Gabriel Gonzalez <gabokappa@gmail.com>
2019-08-23 14:46:54 +0100

    ex18 first test passed

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,6 +1,14 @@
 
 class Bike
-  def working?
-    return true
+  # def working?
+  #   true
+  # end
+  def report_broken
+    @broken = true
   end
+
+  def broken?
+    @broken
+  end
+
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,7 +2,17 @@ require_relative "../lib/boris_bikes"
 require_relative "../lib/bike"
 
 describe Bike do
-  it "responds to working?" do
-    expect(Bike.new).to respond_to(:working?)
+  # it "responds to working?" do
+  #   expect(Bike.new).to respond_to(:working?)
+  # end
+
+  it 'can be reported broken' do
+    subject.report_broken
+    expect(subject).to be_broken
   end
+
+ # it "returns whether the bike is working" do
+ #   expect(subject.working?("no")).to eq false
+ #
+ # end
 end
